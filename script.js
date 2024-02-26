b61d90feb816f130f213e0d8379bca3342c1fe3f
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-02-26 14:07:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function isSameType(value1, value2) {
-  //your js code here
-	if(typeof value1===typeof value2){
-		return true;
-	}
-	else{
-		return false;
-	}
-}
+change the code below.
 
-//do not change the code below.
-
-alert(isSameType(1, 3));
-alert(isSameType("hey", "hello"));
-alert(isSameType(NaN, NaN));
-alert(isSameType("3", 3));
-alert(isSameType("3", NaN));
 
 
 
